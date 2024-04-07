b2bed5e2a01fc9dceb7766fc07053fa8ce3dc51c
Yau-Meng Wong <102400085+y4umeng@users.noreply.github.com>
2024-04-07 23:00:05 +0800

    debug

--- NICE-Transeg/datagenerators.py
+++ NICE-Transeg/datagenerators.py
@@ -26,5 +26,5 @@ class NICE_Transeg_Dataset(Dataset):
 def print_gpu_usage(note=""):
     print(note)
     print("torch.cuda.memory_allocated: %fGB"%(torch.cuda.memory_allocated(0)/1024/1024/1024))
-    print("torch.cuda.memory_reserved: %fGB"%(torch.cuda.memory_reserved(0)/1024/1024/1024))
-    print("torch.cuda.max_memory_reserved: %fGB"%(torch.cuda.max_memory_reserved(0)/1024/1024/1024))
+    # print("torch.cuda.memory_reserved: %fGB"%(torch.cuda.memory_reserved(0)/1024/1024/1024))
+    # print("torch.cuda.max_memory_reserved: %fGB"%(torch.cuda.max_memory_reserved(0)/1024/1024/1024))
