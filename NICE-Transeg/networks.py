c7b7401137cb3b6a27454ee0ef69ea92742fedb8
Yau-Meng Wong <102400085+y4umeng@users.noreply.github.com>
2024-04-11 14:25:34 +0800

    finding gpu limit

--- NICE-Transeg/networks.py
+++ NICE-Transeg/networks.py
@@ -51,6 +51,9 @@ class NICE_Transeg(nn.Module):
 
         seg_fix = self.SegmentationDecoder(x_fix)
 
+        print(seg_fix.shape)
+        print(x_fix[0].shape)
+
         flow = flows[0]
         warped = self.SpatialTransformer(moving, flow)
         affined = self.AffineTransformer(moving, affine_para)
