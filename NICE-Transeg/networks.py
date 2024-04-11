46bfda542d55288e0511c1a714220b7da3e43d47
Yau-Meng Wong <102400085+y4umeng@users.noreply.github.com>
2024-04-11 14:29:54 +0800

    finding gpu limit

--- NICE-Transeg/networks.py
+++ NICE-Transeg/networks.py
@@ -334,6 +334,7 @@ class Transeg_decoder(nn.Module):
 
         x = torch.cat([x_1, x_1], dim=1)
         x_1 = self.conv_1(x)
+        print(f"after_conv {x_1.shape}")
         
         x = self.reghead_1(x_1)
         seg_1 = x
