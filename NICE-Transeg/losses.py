b940a94dc52bd19f6447045116bc782541a63641
Yau-Meng Wong <102400085+y4umeng@users.noreply.github.com>
2024-04-15 16:02:55 +0800

    testing njd as loss

--- NICE-Transeg/losses.py
+++ NICE-Transeg/losses.py
@@ -87,7 +87,7 @@ class Grad:
         
         return grad
 
-def NJDinfer(disp):
+def NJD(disp):
     # Negative Jacobian Determinant adapted from TransMorph repo
     disp = np.reshape(disp, (1, 3, 160, 192, 224))
     
@@ -115,31 +115,6 @@ def NJDinfer(disp):
              jacobian[1, 0, :, :, :] * (jacobian[0, 1, :, :, :] * jacobian[2, 2, :, :, :] - jacobian[0, 2, :, :, :] * jacobian[2, 1, :, :, :]) +\
              jacobian[2, 0, :, :, :] * (jacobian[0, 1, :, :, :] * jacobian[1, 2, :, :, :] - jacobian[0, 2, :, :, :] * jacobian[1, 1, :, :, :])
     return np.sum(jacdet<0) / np.prod(jacdet.shape) 
-
-class NJD:
-    def __init__(self, Lambda=1e-5):
-        self.Lambda = Lambda
-        
-    def get_Ja(self, displacement):
-
-        D_y = (displacement[:,1:,:-1,:-1,:] - displacement[:,:-1,:-1,:-1,:])
-        D_x = (displacement[:,:-1,1:,:-1,:] - displacement[:,:-1,:-1,:-1,:])
-        D_z = (displacement[:,:-1,:-1,1:,:] - displacement[:,:-1,:-1,:-1,:])
-
-        D1 = (D_x[...,0]+1)*( (D_y[...,1]+1)*(D_z[...,2]+1) - D_z[...,1]*D_y[...,2])
-        D2 = (D_x[...,1])*(D_y[...,0]*(D_z[...,2]+1) - D_y[...,2]*D_x[...,0])
-        D3 = (D_x[...,2])*(D_y[...,0]*D_z[...,1] - (D_y[...,1]+1)*D_z[...,0])
-        
-        return D1-D2+D3
-
-    def loss(self, _, y_pred):
-
-        displacement = y_pred.permute(0, 2, 3, 4, 1)
-        Ja = self.get_Ja(displacement)
-        Neg_Jac = 0.5*(torch.abs(Ja) - Ja)
-    
-        return self.Lambda*torch.sum(Neg_Jac)
     
 def Regu_loss(y_true, y_pred):
-    # return Grad('l2').loss(y_true, y_pred)  # Disable NJD loss if PyTorch>1.10
-    return Grad('l2').loss(y_true, y_pred) + NJD(1e-5).loss(y_true, y_pred)
+    return Grad('l2').loss(y_true, y_pred) + 1e-5 * NJD(y_pred)
--- NICE-Transeg/train.py
+++ NICE-Transeg/train.py
@@ -154,7 +154,6 @@ def train(train_dir,
         valid_Dice = []
         valid_Affine = []
         valid_NJD = []
-        test_NJD = 0
         for valid_images, valid_labels in valid_dl:
 
             fixed_vol = valid_images[0][None,...].float()
@@ -178,9 +177,7 @@ def train(train_dir,
             Affine_val = Dice(affine_seg, fixed_seg)
             valid_Affine.append(Affine_val)
 
-            flow = pred[1].detach().cpu()
-            test_NJD = losses.NJD(1).loss(None, flow)
-            flow = flow.permute(0, 2, 3, 4, 1).numpy().squeeze()
+            flow = pred[1].detach().cpu().permute(0, 2, 3, 4, 1).numpy().squeeze()
             NJD_val = losses.NJDinfer(flow)
             valid_NJD.append(NJD_val)
             break
@@ -194,7 +191,6 @@ def train(train_dir,
         valid_Affine_info = 'Valid affine DSC: %.4f' % (np.mean(valid_Affine))
         valid_NJD_info = 'Valid NJD: %.5f' % (np.mean(valid_NJD))
         print(' - '.join((epoch_info, time_info, train_loss_info, valid_Dice_info, valid_Affine_info, valid_NJD_info)), flush=True)
-        print(f"TEST NJD: {test_NJD}")
         # save model checkpoint
         torch.save(model.state_dict(), os.path.join(model_dir, '%02d.pt' % (epoch+1)))
     
