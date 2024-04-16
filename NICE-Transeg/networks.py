497c855ef393b00761bfb628028fbf877921783b
Yau-Meng Wong <102400085+y4umeng@users.noreply.github.com>
2024-04-16 15:15:32 +0800

    working on seg branch

--- NICE-Transeg/networks.py
+++ NICE-Transeg/networks.py
@@ -32,7 +32,7 @@ class NICE_Transeg(nn.Module):
                                      channel_num=dec_channels, 
                                      use_checkpoint=use_checkpoint)
         self.SegmentationDecoder = Transeg_decoder(in_channels=enc_channels,
-                                     channel_num=num_classes,
+                                     channel_num=dec_channels,
                                      use_checkpoint=use_checkpoint)
         
         self.SpatialTransformer = SpatialTransformer_block(mode='bilinear')
