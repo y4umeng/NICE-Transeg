1477b4fe9b87e594bc1985770b584028e8ea25bc
Yau-Meng Wong <102400085+y4umeng@users.noreply.github.com>
2024-04-18 19:07:03 +0800

    debug loss

--- NICE-Transeg/losses.py
+++ NICE-Transeg/losses.py
@@ -118,10 +118,8 @@ class NJD:
         self.eye = torch.eye(3, 3).reshape(3, 3, 1, 1, 1).to(device)
         
     def loss(self, disp):
-        # disp = disp.permute(0, 2, 3, 4, 1) 
-        print(disp.shape)
-        disp = torch.reshape(disp.permute(0, 2, 3, 4, 1) , (1, 3, 160, 192, 224))
-        print(disp.shape)
+        _, _, H, W, D = disp.shape # 1, 3, 160, 192, 224
+        disp = torch.reshape(disp.permute(0, 2, 3, 4, 1) , (1, 3, H, W, D))
 
         gradx_disp = torch.stack([self.gradx(disp[:, i, :, :, :]) for i in range(3)], axis = 1)
         grady_disp = torch.stack([self.grady(disp[:, i, :, :, :]) for i in range(3)], axis = 1)
