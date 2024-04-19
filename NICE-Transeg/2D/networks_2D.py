04c5f9407fdb9dba0b472b8517a8ba8dffef7b4d
Yau-Meng Wong <102400085+y4umeng@users.noreply.github.com>
2024-04-19 16:27:46 +0800

    testing increasing channel num

--- NICE-Transeg/2D/networks_2D.py
+++ NICE-Transeg/2D/networks_2D.py
@@ -18,8 +18,8 @@ class NICE_Transeg(nn.Module):
     def __init__(self, 
                  num_classes: int = 36,
                  in_channels: int = 1, 
-                 enc_channels: int = 160, 
-                 dec_channels: int = 320, 
+                 enc_channels: int = 32, 
+                 dec_channels: int = 64, 
                  use_checkpoint: bool = True,
                  verbose: bool = False
                  ):
